5214125e2bc633d681a6f1454c251163cb748287
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-04-23 14:20:18 -0400

    quickSort

---
+++ quickSort.js
@@ -0,0 +1,12 @@
+function quickSort(arr) {
+  if (!arr.length) return [];
+  let left = [];
+  let right = [];
+  let pivot = arr[arr.length - 1];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+  
+  return quickSort(left).concat([pivot]).concat(quickSort(right));
+}
