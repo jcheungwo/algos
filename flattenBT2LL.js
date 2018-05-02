53cc1e9077b3b054d1ae0015a3c215acc96ec770
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-05-02 16:09:19 -0400

    modify BT2LL solution

--- flattenBT2LL.js
+++ flattenBT2LL.js
@@ -1,14 +1,14 @@
 let flatten = function(root) {
   // return if root does not exist
   if (!root) return;
-  // swap left and right
-  [root.left, root.right] = [root.right, root.left];
   // call function on right
   flatten(root.right);
   // call function on left
   flatten(root.left);
   // go to next available right and place left in it
   if (root.left) {
+    // swap left and right
+    [root.left, root.right] = [root.right, root.left];
     let currentNode = root;
     while (currentNode.right) currentNode = currentNode.right;
     currentNode.right = root.left;
