22261b64f5b708af27a10cfc9932b8dc50d38c58
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-05-07 12:45:37 -0400

    merge range

---
+++ InterviewCake/04 mergeRange.js
@@ -0,0 +1,82 @@
+function mergeRanges(meetings) {
+  // Merge meetings ranges
+  
+  let returnArray;
+  //sort by start time
+  meetings = meetings.sort((a, b) => a.startTime - b.startTime);
+  returnArray = [meetings[0]];
+  //
+  for (let i = 0; i < meetings.length; i++) {
+    let lastEl = returnArray.length - 1;
+    if (meetings[i].startTime <= returnArray[lastEl].endTime) {
+      returnArray[lastEl].endTime = Math.max(returnArray[lastEl].endTime, meetings[i].endTime);
+    }
+    else returnArray.push(meetings[i]);
+  }
+  return returnArray;
+}
+
+// Tests
+
+let desc = 'meetings overlap';
+let actual = mergeRanges([{startTime: 1, endTime: 3}, {startTime: 2, endTime: 4}]);
+let expected = [{startTime: 1, endTime: 4}];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings touch';
+actual = mergeRanges([{startTime: 5, endTime: 6}, {startTime: 6, endTime: 8}]);
+expected = [{startTime: 5, endTime: 8}];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meeting contains other meeting';
+actual = mergeRanges([{startTime: 1, endTime: 8}, {startTime: 2, endTime: 5}]);
+expected = [{startTime: 1, endTime: 8}];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings stay separate';
+actual = mergeRanges([{startTime: 1, endTime: 3}, {startTime: 4, endTime: 8}]);
+expected = [{startTime: 1, endTime: 3}, {startTime: 4, endTime: 8}];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'multiple merged meetings';
+actual = mergeRanges([
+  {startTime: 1, endTime: 4},
+  {startTime: 2, endTime: 5},
+  {startTime: 5, endTime: 8},
+]);
+expected = [{startTime: 1, endTime: 8}];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings not sorted';
+actual = mergeRanges([
+  {startTime: 5, endTime: 8},
+  {startTime: 1, endTime: 4},
+  {startTime: 6, endTime: 8},
+]);
+expected = [{startTime: 1, endTime: 4}, {startTime: 5, endTime: 8}];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'sample input';
+actual = mergeRanges([
+  {startTime: 0, endTime: 1},
+  {startTime: 3, endTime: 5},
+  {startTime: 4, endTime: 8},
+  {startTime: 10, endTime: 12},
+  {startTime: 9, endTime: 10}, 
+]);
+expected = [
+  {startTime: 0, endTime: 1},
+  {startTime: 3, endTime: 8},
+  {startTime: 9, endTime: 12},
+];
+assertArrayEquals(actual, expected, desc);
+
+function assertArrayEquals(a, b, desc) {
+  const arrayA = JSON.stringify(a);
+  const arrayB = JSON.stringify(b);
+  if (arrayA !== arrayB) {
+    console.log(`${desc} ... FAIL: ${arrayA} != ${arrayB}`)
+  } else {
+    console.log(`${desc} ... PASS`);
+  }
+}
