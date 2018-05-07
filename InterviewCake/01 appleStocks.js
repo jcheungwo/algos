4e1ac5b34e9281bd1b0b1f6ae3dabb2d44bfa39d
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-05-07 12:59:50 -0400

    added tests

--- InterviewCake/01 appleStocks.js
+++ InterviewCake/01 appleStocks.js
@@ -1,10 +1,61 @@
-let maxProfit = function(prices) {
-  let max = 0;
-  if (prices.length < 2) return max;
-  for (let i = 1, min = prices[0]; i < prices.length; i++) {
-    let current = prices[i];
+function getMaxProfit(stockPrices) {
+  let max;
+  if (stockPrices.length < 2) throw 'error';
+  max = stockPrices[1] - stockPrices[0];
+  for (let i = 1, min = stockPrices[0]; i < stockPrices.length; i++) {
+    let current = stockPrices[i];
     max = Math.max(max, current - min);
     min = Math.min(min, current);
   }
   return max;
 };
+
+let desc = 'price goes up then down';
+let actual = getMaxProfit([1, 5, 3, 2]);
+let expected = 4;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes down then up';
+actual = getMaxProfit([7, 2, 8, 9]);
+expected = 7;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes up all day';
+actual = getMaxProfit([1, 6, 7, 9]);
+expected = 8;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes down all day';
+actual = getMaxProfit([9, 7, 4, 1]);
+expected = -2;
+assertEqual(actual, expected, desc);
+
+desc = 'price stays the same all day';
+actual = getMaxProfit([1, 1, 1, 1]);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'one price throws error';
+const onePrice = () => (getMaxProfit([1]));
+assertThrowsError(onePrice, desc);
+
+desc = 'empty array throws error';
+const emptyArray = () => (getMaxProfit([]));
+assertThrowsError(emptyArray, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
--- InterviewCake/03 3numMaxProduct.js
+++ InterviewCake/03 3numMaxProduct.js
@@ -1,10 +1,10 @@
-let maxProductOfThree = function(array) {
+function highestProductOf3(array) {
   // O(n) Time, O(1) space
 
   //store 3 largest numbers, 2 smallest numbers
   let largest1 = array[0], smallest1 = array[0];
   let largest2, largest3, smallest2;
-  if (array.length < 3) return null;
+  if (array.length < 3) throw 'error';
   for (let i = 1; i < array.length; i++) {
     if (array[i] >= largest1) {
       [largest3, largest2, largest1] = [largest2, largest1, array[i]];
@@ -38,4 +38,60 @@ let maxProductOfThree = function(array) {
   }
   //otherwise return 3 greatest numbers
   return largest1 * largest2 * largest3;
+}
+
+// Tests
+
+let desc = 'short array';
+let actual = highestProductOf3([1, 2, 3, 4]);
+let expected = 24;
+assertEqual(actual, expected, desc);
+
+desc = 'longer array';
+actual = highestProductOf3([6, 1, 3, 5, 7, 8, 2]);
+expected = 336;
+assertEqual(actual, expected, desc);
+
+desc = 'array has one negative';
+actual = highestProductOf3([-5, 4, 8, 2, 3]);
+expected = 96;
+assertEqual(actual, expected, desc);
+
+desc = 'array has two negatives';
+actual = highestProductOf3([-10, 1, 3, 2, -10]);
+expected = 300;
+assertEqual(actual, expected, desc);
+
+desc = 'array is all negatives';
+actual = highestProductOf3([-5, -1, -3, -2]);
+expected = -6;
+assertEqual(actual, expected, desc);
+
+desc = 'error with empty array';
+const emptyArray = () => (highestProductOf3([]));
+assertThrowsError(emptyArray, desc);
+
+desc = 'error with one number';
+const oneNumber = () => (highestProductOf3([1]));
+assertThrowsError(emptyArray, desc);
+
+desc = 'error with two numbers';
+const twoNumber = () => (highestProductOf3([1, 1]));
+assertThrowsError(twoNumber, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
 }
