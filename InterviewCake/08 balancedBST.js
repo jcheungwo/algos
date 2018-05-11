47e6d529f0d121860f3e2106b67167710f397c83
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-05-11 12:09:07 -0400

    check if bst is balanced

---
+++ InterviewCake/08 balancedBST.js
@@ -0,0 +1,116 @@
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left  = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+function isBalanced(treeRoot) {
+  let stack = [treeRoot];
+  let leafNodes = [];
+  let currentNode, max, min;
+  treeRoot.level = 1;
+  // breadth first traversal
+  while (stack.length) {
+    currentNode = stack.shift();
+    // add their current level at each node
+    if (currentNode.left) {
+      currentNode.left.level = currentNode.level + 1;
+      stack.push(currentNode.left);
+    }
+      if (currentNode.right) {
+      currentNode.right.level = currentNode.level + 1;
+      stack.push(currentNode.right);
+    }
+    // check their levels
+    if (!currentNode.left && !currentNode.right) {
+      if (!max) {
+        max = min = currentNode.level;
+      } else if (max < currentNode.level) {
+        max = currentNode.level;
+      } else if (min > currentNode.level) {
+        min = currentNode.level;
+      }
+      if (max - min > 1) return false;
+    }
+  }
+  return true;
+}
+
+// Tests
+
+let desc = 'full tree';
+let treeRoot = new BinaryTreeNode(5);
+let leftNode = treeRoot.insertLeft(8);
+leftNode.insertLeft(1);
+leftNode.insertRight(2);
+let rightNode = treeRoot.insertRight(6);
+rightNode.insertLeft(3);
+rightNode.insertRight(4);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'both leaves at the same depth';
+treeRoot = new BinaryTreeNode(3);
+leftNode = treeRoot.insertLeft(4);
+leftNode.insertLeft(1);
+rightNode = treeRoot.insertRight(6);
+rightNode.insertRight(9);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'leaf heights differ by one';
+treeRoot = new BinaryTreeNode(6);
+leftNode = treeRoot.insertLeft(1);
+rightNode = treeRoot.insertRight(0);
+rightNode.insertRight(7);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'leaf heights differ by two';
+treeRoot = new BinaryTreeNode(6);
+leftNode = treeRoot.insertLeft(1);
+rightNode = treeRoot.insertRight(0);
+rightNode.insertRight(7).insertRight(8);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'three leaves total';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(5);
+rightNode = treeRoot.insertRight(9);
+rightNode.insertLeft(8);
+rightNode.insertRight(5);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'both subtrees superbalanced';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(5);
+rightNode = treeRoot.insertRight(9);
+rightNode.insertLeft(8).insertLeft(7);
+rightNode.insertRight(5);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'only one node';
+treeRoot = new BinaryTreeNode(1);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'linked list tree';
+treeRoot = new BinaryTreeNode(1);
+treeRoot.insertRight(2).insertRight(3).insertRight(4);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+function assertEquals(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
